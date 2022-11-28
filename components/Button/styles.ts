bb6bf55515b96d5b4826fd407e2e6102b7d94be8
Matheus Andrade <2002matheus@gmail.com>
2022-11-28 11:50:02 -0300

    add bookNow

---
+++ components/Button/styles.ts
@@ -0,0 +1,21 @@
+import tw from "tailwind-styled-components"
+
+interface ButtonProps {
+  px?: number
+  py?: number
+  shadow?: 1 | 0
+}
+
+export const Button = tw.button<ButtonProps>`
+  bg-purplePrimary
+  text-white
+  rounded-full
+  font-semibold
+
+  hover:bg-hoverPurplePrimary
+
+  drop-shadow-custom
+
+  ${({ px }) => (px ? `px-${px}` : "")}
+  ${({ py }) => (py ? `py-${py}` : "")}
+`
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
+
 import * as TC from "../styles/styles"
 
 import AirplaneImage from "../assets/airplane.svg"
 import AviationText from "../assets/AviationText.png"
+import { CiLocationOn } from "react-icons/ci"
+import { BsPeople } from "react-icons/bs"
+import { HiOutlineCalendar } from "react-icons/hi"
+
+import { Button } from "../components/Button/styles"
+import { BookNowOptions } from "../components/BookNowOptions"
 
 export default function Home() {
+  const [bookNowOption, setBookNowOption] = useState(0)
+
   return (
     <TC.Container>
       <TC.Background />
@@ -24,6 +34,55 @@ export default function Home() {
           having surprising discoveries
         </TC.SubText>
       </TC.Presentation>
+      <TC.BookNowWrapper>
+        <TC.BookNowOptions>
+          <TC.BookNowOption
+            onClick={() => setBookNowOption(0)}
+            active={bookNowOption === 0 ? 1 : 0}>
+            Economy
+          </TC.BookNowOption>
+          <TC.BookNowOption
+            onClick={() => setBookNowOption(1)}
+            active={bookNowOption === 1 ? 1 : 0}>
+            Busniness
+          </TC.BookNowOption>
+          <TC.BookNowOption
+            onClick={() => setBookNowOption(2)}
+            active={bookNowOption === 2 ? 1 : 0}>
+            First
+          </TC.BookNowOption>
+        </TC.BookNowOptions>
+        <TC.BookNowContent>
+          <TC.BookNowTop>
+            <BookNowOptions
+              icon={<CiLocationOn className="h-6 w-6" />}
+              title={"Location"}
+              subTitle={"Where are you going?"}
+            />
+            <BookNowOptions
+              icon={<BsPeople className="h-6 w-6" />}
+              title={"Travelers"}
+              subTitle={"Add Guest"}
+            />
+            <BookNowOptions
+              icon={<HiOutlineCalendar className="h-6 w-6" />}
+              title={"Check-in"}
+              subTitle={"Add date"}
+            />
+            <BookNowOptions
+              icon={<HiOutlineCalendar className="h-6 w-6" />}
+              title={"Check-out"}
+              subTitle={"Add date"}
+            />
+          </TC.BookNowTop>
+          <Button
+            px={10}
+            py={4}
+            shadow={1}>
+            Book Now
+          </Button>
+        </TC.BookNowContent>
+      </TC.BookNowWrapper>
     </TC.Container>
   )
 }
