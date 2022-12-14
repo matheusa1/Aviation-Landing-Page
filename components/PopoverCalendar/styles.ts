e661ccf9a033066609a78c782ebc0d9116cf5f79
Matheus Andrade <2002matheus@gmail.com>
2022-12-14 16:23:05 -0300

    fix check-out calendar

--- components/PopoverCalendar/styles.ts
+++ components/PopoverCalendar/styles.ts
@@ -1,3 +1,18 @@
 import tw from 'tailwind-styled-components'
 
-export const Container = tw.div``
+export const Container = tw.div`
+  bg-white
+  flex
+  flex-col
+  gap-4
+  items-center
+  p-2
+  shadow-2xl
+  rounded-xl
+`
+
+export const Alert = tw.span`
+  text-sm
+  font-bold
+  text-red-500
+`
